e1a8c5b83d10fc4cd7349eb88e6689649e3bf16f
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-03-26 12:49:37 +0300

    arrays lab complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee ={
+    name:'ephy',
+    streetAddress:'58 hamptons'
+}
+console.log(Object.keys(employee))
+
+function updateEmployeeWithKeyAndValue(employee,key,value,){
+const newEmployee = {...employee}
+    newEmployee[key]=value
+console.log(newEmployee)
+return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value
+    return employee
+}
+function deleteFromEmployeeByKey(employee,key){
+    const newEmployee2 = {...employee}
+    delete newEmployee2[key]
+    return newEmployee2
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+   delete employee[key]
+    return employee
+}
